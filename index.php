ad9bb6b43f8e996d3061c55d2f15e7765088fad4
parthapratimsarkar1 <150425513+parthapratimsarkar1@users.noreply.github.com>
2024-08-17 15:46:11 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html lang = "en">
+	<head>
+		<title>Hotel Online Reservation</title>
+		<meta charset = "utf-8" />
+		<meta name = "viewport" content = "width=device-width, initial-scale=1.0" />
+		<link rel = "stylesheet" type = "text/css" href = "css/bootstrap.css " />
+		<link rel = "stylesheet" type = "text/css" href = "css/style.css" />
+	</head>
+<body>
+	<nav style = "background-color:rgba(0, 0, 0, 0.1);" class = "navbar navbar-default">
+		<div  class = "container-fluid">
+			<div class = "navbar-header">
+				<a class = "navbar-brand" >Hotel Online Reservation</a>
+			</div>
+		</div>
+	</nav>	
+	<ul id = "menu">
+		<li><a href = "index.php">Home</a></li> |
+		<li><a href = "aboutus.php">About us</a></li> |
+		<li><a href = "contactus.php">Contact us</a></li> |
+		<li><a href = "gallery.php">Gallery</a></li> |
+		<li><a href = "dineandlounge.php">Dine and Lounge</a></li> |			
+		<li><a href = "reservation.php">Make a reservation</a></li> |
+		<li><a href = "rulesandregulation.php">Rules and Regulation</a></li>
+	</ul>
+	<div id="myCarousel" class="carousel slide container-fluid" data-ride="carousel">
+		<ol class="carousel-indicators">
+			<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+			<li data-target="#myCarousel" data-slide-to="1"></li>
+			<li data-target="#myCarousel" data-slide-to="2"></li>
+			<li data-target="#myCarousel" data-slide-to="3"></li>
+			<li data-target="#myCarousel" data-slide-to="4"></li>
+			<li data-target="#myCarousel" data-slide-to="5"></li>
+			<li data-target="#myCarousel" data-slide-to="6"></li>
+		</ol>
+		<div style = "margin:auto;" class="carousel-inner" role="listbox">
+			<div class="item active">
+				<img src="images/a.jpg" style = "width:100%; height:450px;" />
+			</div>
+		
+			<div class="item">
+				<img src="images/b.jpg" style = "width:100%; height:450px;"  />
+			</div>
+		
+			<div class="item">
+				<img src="images/c.jpg" style = "width:100%; height:450px;" />
+			</div>
+		
+			<div class="item">
+				<img src="images/d.jpg" style = "width:100%; height:450px;" />
+			</div>
+			
+			<div class="item">
+				<img src="images/e.jpg" style = "width:100%; height:450px;" />
+			</div>
+			
+			<div class="item">
+				<img src="images/f.jpeg" style = "width:100%; height:450px;" />
+			</div>
+			
+			<div class="item">
+				<img src="images/g.png" style = "width:100%; height:450px;" />
+			</div>
+			
+			
+		</div>
+		<a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+			<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+			<span class="sr-only">Previous</span>
+		</a>
+		<a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+			<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+			<span class="sr-only">Next</span>
+		</a>	
+	</div>
+	<br />
+	<br />
+	<div style = "text-align:right; margin-right:10px;" class = "navbar navbar-default navbar-fixed-bottom">
+		<label>&copy; Tezpur University </label>
+	</div>
+
+   <div class="col-sm-6">
+       <h1><font style= "font-family: Lucida Console, Monaco, monospace">Welcome to Our Hotel Reservation System </font>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+       <font style="color: #0000cc; font-size: 37px;"> </font></h1><p>&nbsp;</p>
+    </div>
+    <div class="col-sm-11">
+       <p style="font-size: 17px;font-family: Arial, Helvetica, sans-serif">Welcome to OUR HOTEL where business and leisure blend together. Enjoying an unrivalled location, overlooking the Bay of Bengal (only 25 yards from the Bay water) and sitting in the laps of hills, we offer deluxe accommodation in 181 well appointed guest rooms and suites. The panoramic view of the ocean, the majestic hills and the natural beauty of the tamarisk trees are all wonderfully complemented by luxury facilities and Indian hospitality. - <font style="color: #0000cc">See more at: http://www.facebook.com</font></p><p>&nbsp;</p><p>&nbsp;</p>
+    </div>
+
+
+
+ <div class="row">
+    <div class="col-sm-12">
+    <h1 style="font-family: Courier New, Courier, monospace">Hotel Facilities</h1>
+    <hr style="color: #000000; border-width: 3px;">
+  </div>
+    <div class="col-sm-6"> 
+      <ul>
+        <li><p>Central air conditioning</p></li>
+        <li><p>Hot &amp; cold water</p></li>
+        <li><p>Cable television with over 84 channels </p></li>
+        <li><p>Comfortable working desk with table lamp </p></li>
+        <li><p>High Speed Internet access </p></li>
+        <li><p>In room safe (suite only)</p></li>
+        <li><p>Doctor on call</p></li>
+        <li><p>24 hours room services</p></li>
+        <li><p>Conference and Banquet facility</p></li>
+        <li><p>Airport transfer</p></li>
+      </ul>
+    </div>
+
+    <div class="col-sm-6">
+      <ul>
+        <li><p>Mini bar</p></li>
+        <li><p>Business Centre</p></li>
+        <li><p>Free parking</p></li>
+        <li><p>IDD Telephone</p></li>
+        <li><p>Hairdresser</p></li>
+        <li><p>Fitness centre</p></li>
+        <li><p>Shopping Arcade</p></li>
+        <li><p>Hot Spa &amp; Massage</p></li>
+        <li><p>Swimming pool</p></li>
+        <li><p>Cyber Cafe</p></li>    
+
+
+
+
+                             
+      </ul>
+    </div>
+
+
+
+
+
+</body>
+<script src = "js/jquery.js"></script>
+<script src = "js/bootstrap.js"></script>	
+</html>
